bb21dac48a4412c4d7902663ed79c92395e94dbb
Aslam554 <begaslam405@gmail.com>
2026-05-16 17:54:02 +0530

    fix in jinja2 hoem route

--- 00 practice-project/main.py
+++ 00 practice-project/main.py
@@ -1,34 +1,34 @@
-from fastapi import FastAPI,Request
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from students import student
 from fastapi.templating import Jinja2Templates
-
+from students import student
 
 app = FastAPI()
-templates = Jinja2Templates(directory='templates')
 
-#home route /
-# @app.get("/",response_class=HTMLResponse)
-# def home():
-#     return f"<h1>Welcome to Home Page</h1>"
+templates = Jinja2Templates(directory="templates")
 
-#jinja2 templating for rendering html pages directly
+
+# Home Route
 @app.get("/", include_in_schema=False)
 def home(request: Request):
 
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
-    )
+    return templates.TemplateResponse(request,"index.html")
 
 
-#get all students details
-@app.get("/api/students",response_class=HTMLResponse,include_scheme=False)
+# Get All Students
+@app.get(
+    "/api/students",
+    response_class=HTMLResponse,
+    include_in_schema=False
+)
 def get_students():
+
     data = ""
+
     for i in student:
-       data+= f'''
-        <div style='background-color:blue; color:white;padding:5px;width:40%'>
+
+        data += f"""
+        <div style='background-color:blue; color:white; padding:5px; width:40%'>
           <ul>
             <li>Id : {i['id']}</li>
             <li>Name : {i['name']}</li>
@@ -36,26 +36,34 @@ def get_students():
             <li>Gender : {i['gender']}</li>
             <li>About : {i['about']}</li>
           </ul>
-       </div>
-       <hr/>
-      '''
+        </div>
+
+        <hr/>
+        """
+
     return data
 
-#get a single student data now
-@app.get("/api/students/{id}",response_class=HTMLResponse)
-def single_student(id:int):
+
+# Get Single Student By ID
+@app.get("/api/students/{id}", response_class=HTMLResponse)
+def single_student(id: int):
 
     for i in student:
-        if i['id']==id:
-         return f'''
-          <div style='background-color:blue; color:white;padding:5px;width:40%'>
-            <ul>
-                <li>Id : {i['id']}</li>
-                <li>Name : {i['name']}</li>
-                <li>Age : {i['age']}</li>
-                <li>Gender : {i['gender']}</li>
-                <li>About : {i['about']}</li>
-            </ul>
-          </div>
-         '''
-    return f'<h1>Student with {id} is not found here</h1>'
+
+        if i["id"] == id:
+
+            return f"""
+            <div style='background-color:blue; color:white; padding:5px; width:40%'>
+
+                <ul>
+                    <li>Id : {i['id']}</li>
+                    <li>Name : {i['name']}</li>
+                    <li>Age : {i['age']}</li>
+                    <li>Gender : {i['gender']}</li>
+                    <li>About : {i['about']}</li>
+                </ul>
+
+            </div>
+            """
+
+    return f"<h1>Student with ID {id} not found</h1>"
